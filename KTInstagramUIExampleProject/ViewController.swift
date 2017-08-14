372257c698b720d88390c2d16751ae3f9ce27a1e
Craig Holliday <themisterholliday@gmail.com>
2017-08-14 13:54:51 -0500

    Initial Commit

---
+++ KTInstagramUIExampleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KTInstagramUIExampleProject
+//
+//  Created by Craig Holliday on 8/14/17.
+//  Copyright © 2017 Koala Tea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
